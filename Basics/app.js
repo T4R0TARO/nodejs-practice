b7132341191827c9166e907e56166f60dbd592e4
josh manansala <joshmanansalagsd@gmail.com>
2023-09-06 13:24:11 -0700

    node practice

--- Basics/app.js
+++ Basics/app.js
@@ -2,17 +2,6 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   //   console.log(req);
-  //   if (req.url === "/") {
-  //     res.end("Welcome to our homepage");
-  //   }
-  //   if (req.url === "/about") {
-  //     res.end("Here is our short history");
-  //   }
-  //   res.end(`
-  //     <h1>404 Error</h1>
-  //     <p>Sumimasori... Something went wrong</p>
-  //     <a href="/">back home</a>
-  //     `);
 
   //   res.write("Welcome to our homepage");
   //   res.end();
