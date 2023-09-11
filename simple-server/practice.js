0247ac879a6c687a43dc0e6bd32b6fd314e6e0e1
josh manansala <joshmanansalagsd@gmail.com>
2023-09-10 18:31:10 -0700

    express basics practice

--- simple-server/practice.js
+++ simple-server/practice.js
@@ -2,18 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// create a public folder to hold copies of static files
 app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./server/index.html"));
+  res.sendFile(path.resolve(__dirname, "./server.index.html"));
 });
 
 app.all("*", (req, res) => {
-  res.status(404).send("Resource was not found");
+  res.status(404).send("Resource not found...");
 });
 
 app.listen(5000, () => {
-  console.log("The server is listening on port 5000...");
+  console.log("Server is listening on port 5000");
 });
-
-// ||
