5bf8e02f47f42569471f3c1c8620490ff79f02d7
josh manansala <joshmanansalagsd@gmail.com>
2023-09-12 11:25:22 -0700

    node express practice

--- simple-server/app.js
+++ simple-server/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+// import data
 const { holoMyth } = require("./data");
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Homepage</h1> <a href="/api/talents">HoloMyth</a>`);
+  res.send(`<h1>Homepage</h1><a href="/api/talents">HoloMyth</a>`);
 });
 
+// GET talents data w/ specified data
 app.get("/api/talents", (req, res) => {
   const genTalents = holoMyth.map((talent) => {
     const { id, name, mascot } = talent;
@@ -14,21 +16,24 @@ app.get("/api/talents", (req, res) => {
   res.json(genTalents);
 });
 
+// GET singleTalent data
 app.get("/api/talents/:talentID", (req, res) => {
   const { talentID } = req.params;
-
   const singleTalent = holoMyth.find(
     (talent) => talent.id === Number(talentID)
   );
-
+  // Error handling
   if (!singleTalent) {
-    res.status(404).send("Talent ID was not found");
+    res.status(404).send(`Talent ID was not found`);
   }
-
   return res.json(singleTalent);
+});
+
+app.get("/api/talents/:talentID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("Hello World...");
 });
 
 app.listen(5000, () => {
   console.log(`Server listening on port 5000...`);
 });
-// |||
