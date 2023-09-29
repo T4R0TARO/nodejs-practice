08268487fbdda85b84004dea7c6171c531f9e97e
josh manansala <joshmanansalagsd@gmail.com>
2023-09-28 22:32:12 -0700

    fix data leak

--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -1,9 +1,5 @@
 const mongoose = require("mongoose");
 
-// update password and db name in str
-const connectionString =
-  "mongodb+srv://jmanansaladev:Takodachi1@nodeexpressprojects.shf6xbn.mongodb.net/task-manager-test?retryWrites=true&w=majority";
-
 // mongoose v 6.00 and above / no need to fix deprecation warnings
 const connectDB = (url) => {
   return mongoose
