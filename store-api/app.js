5c67b3744737424b95f5975ec073e1ddc4bd2e0f
josh manansala <joshmanansalagsd@gmail.com>
2023-10-20 22:54:09 -0700

    store-api folder and file setup

---
+++ store-api/app.js
@@ -0,0 +1,16 @@
+/**
+ * TODO: npm install
+ * TODO: npm init
+ * TODO: npm install ...
+ * *Dependencies
+ * dotenv
+ * express
+ * express-async-errors
+ * mongodb
+ * mongoose
+ *
+ * *DevDependencies
+ * nodemon
+ * TODO: .gitignore
+ * TODO: .env
+ */
