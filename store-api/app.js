c76d108e73639b010b23f18423aaeebc0baa896d
josh manansala <joshmanansalagsd@gmail.com>
2023-10-21 18:53:58 -0700

    npm setup and simple server

--- store-api/app.js
+++ store-api/app.js
@@ -1,16 +1,35 @@
-/**
- * TODO: npm install
- * TODO: npm init
- * TODO: npm install ...
- * *Dependencies
- * dotenv
- * express
- * express-async-errors
- * mongodb
- * mongoose
- *
- * *DevDependencies
- * nodemon
- * TODO: .gitignore
- * TODO: .env
- */
+const express = require("express");
+const app = express();
+const notFoundMiddleWare = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
+
+// TODO: connect to mongoDB
+// TODO: set up routes
+// TODO: set up controllers
+// TODO: set up mongoose schema models
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">product route</a>');
+});
+
+// products route
+app.use(notFoundMiddleWare);
+app.use(errorHandlerMiddleware);
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    // await connectDB();
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
