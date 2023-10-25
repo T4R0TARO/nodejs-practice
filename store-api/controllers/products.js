dff8cbf17ac13edf01db8216b89c5766cfa7a787
josh manansala <joshmanansalagsd@gmail.com>
2023-10-24 20:02:39 -0700

    add query properties

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,11 +1,46 @@
 const Product = require("../models/product");
 
+/**
+ * Why no `try/catch` ??
+ * We implemented a npm package `express-async-errors` that handles errors for us
+ * Now we can write cleaner async function()
+ */
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "Get all products static" });
+  const search = "ab";
+  const products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Get all products..." });
+  const { featured, company, name } = req.query; // destructure from query
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false; //change string to boolean
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  console.log(req.query);
+  // { featured: 'true', company: 'ikea', name: 'a' }
+  console.log(queryObject);
+  /* 
+    {
+      featured: true,
+      company: 'ikea',
+      name: { '$regex': 'a', '$options': 'i' }
+    }
+  */
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
