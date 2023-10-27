c194760156387d91cfb43369c84a4855901ef424
josh manansala <joshmanansalagsd@gmail.com>
2023-10-26 23:37:29 -0700

    page select feature

--- store-api/practice.js
+++ store-api/practice.js
@@ -1,32 +1,47 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "ab";
-  const products = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
+  // const search = "ab";
+  // const products = await Product.find({
+  //   name: { $regex: search, $options: "i" },
+  // });
+
+  // Task: Sort Feature
+  // Sort through json data by name and price
+  const products = await Product.find({}).sort("-name price"); // sort by name and price
   res.status(200).json({ products, nbHits: products.length });
 };
 
+// Task: Sort Feature
+// Sort through json data based on query
+// localhost:3000/api/v1/products?sort=name,price
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query; // destructure from query string
-  const queryObject = {}; //init new empty obj
+  const { featured, company, name, sort } = req.query; // destructure `sort` from query string
+  const queryObject = {};
 
-  // add featured key to queryObject and set string value to boolean
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
-  // add company key to queryObject and set value
   if (company) {
     queryObject.company = company;
   }
-  // add name key to queryObject and set $regex value
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
-  // find(queryObject)
-  const products = await Product.find(queryObject);
+  // const products = await Product.find(queryObject);
+  let result = Product.find(queryObject); // refactored why? We want to run results through sorting conditional
+
+  // if `sort` is in the query
+  if (sort) {
+    // console.log(sort); //-name,price
+    // localhost:3000/api/v1/products?sort=name,price
+    const sortList = sort.split(",").join(" "); //split sort string and join w/ space : -name price
+    result = result.sort(sortList); // sort json data w/ name price
+  } else {
+    result = result.sort("createdAt"); // sort json data w/ "createdAt"
+  }
+  const products = await result; // after conditional await results
   res.status(200).json({ products, nbHits: products.length });
 };
 
