4a60ca5be094fa6ce1626edbba344710c63c1c02
josh manansala <joshmanansalagsd@gmail.com>
2023-11-29 19:02:51 -0800

    jwt feature added to userSchema

--- jobs-api/controllers/auth.js
+++ jobs-api/controllers/auth.js
@@ -1,26 +1,29 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
-const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-  // res.send("register user...");
-
-  // const { name, email, password } = req.body;
-  // if (!name || !email || !password) {
-  //   throw new BadRequestError("Please provide name, email and password");
-  // }
-
   // REFACTOR TO MIDDLEWARE
   // const { name, email, password } = req.body;
   // const salt = await bcrypt.genSalt(10); // random byts
   // const hashedPassword = await bcrypt.hash(password, salt);
   // const tempUser = { name, email, password: hashedPassword };
-
   // const user = await User.create({ ...tempUser });
 
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+
+  // const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+  //   expiresIn: "30d",
+  // });
+
+  // res.status(StatusCodes.CREATED).json({ user });
+
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    // .json({ user: { name: user.getName() }, token });
+    .json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
