0819a7ca41420724656c4eabd846b14e4ee0e7f4
josh manansala <joshmanansalagsd@gmail.com>
2023-12-13 18:55:04 -0800

    update getAllJobs controller

--- jobs-api/controllers/jobs.js
+++ jobs-api/controllers/jobs.js
@@ -3,7 +3,9 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  // res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
@@ -11,7 +13,9 @@ const getJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-  req.body.createdBy = req.user.userId;
+  console.log(req.body);
+  req.body.createdBy = req.user.userId; // add key `createBy` and have value be the user ID
+  console.log(req.body);
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
