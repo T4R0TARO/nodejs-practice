74cab3bec14640d376f9cf01693a3d4efbd0cf17
josh manansala <joshmanansalagsd@gmail.com>
2023-12-18 17:39:17 -0800

    update updateJob controller

--- jobs-api/controllers/jobs.js
+++ jobs-api/controllers/jobs.js
@@ -35,7 +35,28 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  res.send("update job");
+  // destructure values
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  // error handler
+  if (company === "" || position === "") {
+    throw new BadRequestError("Company or Postion fields cannot be empty");
+  }
+  // findByIdAndUpdate()
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  // error handler
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  // success response
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
