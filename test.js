31e52ac979d3e9e9fc86ffa8cfc1b2fdff765f4e
josh manansala <joshmanansalagsd@gmail.com>
2024-01-03 18:24:03 -0800

    review async/await

--- test.js
+++ test.js
@@ -1,52 +1,88 @@
-/** Class Declaration:
- *  provide a wat to create objects with similar properties and methods
- */
-
-class Animal {
-  // the `contructor` method is used to init obj properties
-  constructor(name, sound) {
-    this.name = name;
-    this.sound = sound;
-  }
-
-  makeSound() {
-    console.log(this.sound);
-  }
-}
-
-// You can create instances of a class using the `new` keyword
-const cat = new Animal("Cat", "Nyah");
-const dog = new Animal("Dog", "Bau Bau");
-
-cat.makeSound();
-
-// Inheritance
-// Allowing you to create a new class that is a modified version of an existing class
-
-class Cat extends Animal {
-  constructor(name, sound, breed) {
-    super(name, sound);
-    this.breed = breed;
-  }
-
-  purr() {
-    console.log("Purring...");
-  }
-}
-
-const Inyah = new Cat("Inyah", "Nyah Nyah");
-
-// Inyah.makeSound();
-// Inyah.purr();
-
-const errorTesting = async () => {
-  try {
-    throw new Error("This is a custom error message...");
-  } catch (error) {
-    console.error(error.name);
-    console.error(error.message);
-    console.error(error.stack);
-  }
+// Async Function Declaration
+const myAsyncFunction = async () => {
+  // code here...
 };
 
-// errorTesting();
+// await promises
+// const fetchData = async () => {
+//   const response = await fetch('https://api.example.com/data');
+//   const data = await response.json();
+//   return data;
+// }
+
+// error handling
+async function fetchData() {
+  try {
+    const response = await fetch("https://api.example.com/data");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+/** Callbacks
+ *  pass a callback function as an argument to an async function
+ *  and that callback function gets called when the operation is compelte `setTimeout()`
+ */
+// function fetchData(callback) {
+//   setTimeout(() => {
+//     const data = "Async data";
+//     callback(data);
+//   }, 1000);
+// }
+
+// fetchData((result) => {
+//   console.log(result);
+// });
+
+// function fetchData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const data = "Async data";
+//       resolve(data);
+//     }, 1000);
+//   });
+// }
+
+// fetchData().then((result) => {
+//   console.log(result);
+// });
+
+// async function fetchData() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       const data = "Async data";
+//       resolve(data);
+//     }, 3000);
+//   });
+// }
+
+// async function fetchDataAndLog() {
+//   console.log("hmu");
+//   const result = await fetchData();
+//   console.log(result);
+// }
+
+// fetchDataAndLog();
+
+// const myPromise = new Promise((resolve, reject) => {
+//   // Do something asynchronous
+//   // If successful, call resolve with the result
+//   // If there's an error, call reject with the error
+// });
+
+// myPromise
+//   .then((result) => {
+//     // Do something successful result
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     // Handle the error if the promise is rejected
+//     console.error(error);
+//   });
+
+//   fetchData()
+//     .then((data) => processData(data))
+//     .then((processedData) => displayData(processedData))
+//     .catch((error) => handlerError(error))
